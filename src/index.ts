be5bb1bef69634921cabb6a4ccd0ada37e1f25f0
Casper <me@casperengelmann.com>
2020-03-24 15:58:35 +0100

    update

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,31 @@
 
 import { exec } from 'child_process'
 import hasYarn from 'has-yarn'
+import {
+  // readFile,
+  writeFile,
+  pathExists,
+  ensureDir,
+} from 'fs-extra'
 
-const newLine = (count = 1) => '\n'.repeat(count)
+const log = (message: string, suffix = '...'): void => {
+  process.stdout.write(`${message.trim()}${suffix}`)
+}
 
-const logWithSpacing = (message: string, spacing = 1) => {
-  console.log(
-    newLine(spacing),
-    message,
-    newLine(spacing),
-  )
+type LogStatus = 'SUCCESS' | 'ERROR'
+
+const logStatus = (status: LogStatus) => {
+  switch (status) {
+    case 'SUCCESS':
+      process.stdout.write(' ✅\n')
+      break
+    case 'ERROR':
+      process.stdout.write(' 🚫\n')
+      break
+
+    default:
+      process.stdout.write(' Status Unknown ❓\n')
+  }
 }
 
 const pkgInstaller = (dev = false) => {
@@ -24,19 +40,11 @@ const pkgInstaller = (dev = false) => {
 const run = (command: string) => new Promise((resolve, reject) => {
   exec(command, (err, stdout, stderr) => {
     if (err) {
+      logStatus('ERROR')
       return reject(err)
     }
 
-    if (['yarn', 'npm'].includes(command.split(' ')[0])) {
-      console.log(`Installed ${
-        command
-          .replace('yarn add', '')
-          .replace('npm install', '')
-          .trim()
-          .split(' ')
-          .join(', ')
-      } successfully`)
-    }
+    logStatus('SUCCESS')
 
     resolve({
       stderr,
@@ -48,16 +56,68 @@ const run = (command: string) => new Promise((resolve, reject) => {
 })
 
 exec(hasYarn() ? 'yarn' : 'npm install', async () => {
-  logWithSpacing('Installing eslint')
+  await ensureDir('components')
+  await ensureDir('layout')
+  await ensureDir('public')
+  await ensureDir('style')
+
+  /*   const pkgPath = `${process.cwd()}/package.json`
+
+  const pkgString = (await readFile(pkgPath)).toString()
+  const pkgJson = JSON.parse(pkgString)
+
+  pkgJson.scripts = {
+    dev: 'next',
+    build: 'next build',
+    start: 'next start',
+  }
+
+  await writeFile(pkgPath, JSON.stringify(pkgJson, null, 2)) */
+
+  console.log('Add the following to your `tsconfig.json`')
+  console.log(`"compilerOptions": {
+  "baseUrl": ".",
+  "paths": {
+    "@/components/*": ["components/*"],
+    "@/layout/*": ["layout/*"],
+    "@/public/*": ["public/*"],
+    "@/style/*": ["style/*"]
+  }
+}`)
+
+  log('Installing eslint')
   await run(`${pkgInstaller(true)} eslint`)
-  logWithSpacing('Installing typescript and typescript parser')
-  await run(`${pkgInstaller(true)} -D typescript @typescript-eslint/parser`)
-  logWithSpacing('Installing required Next packages')
+
+  log('Installing typescript and typescript parser')
+  await run(`${pkgInstaller(true)} typescript @typescript-eslint/parser`)
+
+  log('Installing required Next packages')
   await run(`${pkgInstaller()} next react react-dom`)
-  logWithSpacing('Installing types for Next packages')
+
+  log('Installing types for Next packages')
   await run(`${pkgInstaller(true)} @types/next @types/react @types/react-dom @types/node`)
-  console.log('\nInstalling Sass support\n')
+
+  log('Installing Sass support')
   await run(`${pkgInstaller(true)} sass`)
 
-  console.log('Don\'t forget to run `npx eslint`')
+  if (!await pathExists('test-next-config.js')) {
+    await writeFile('test-next.config.js', `
+      /* eslint-disable no-param-reassign */
+
+      const path = require('path')
+
+      module.exports = {
+        webpack: (config) => {
+          config.resolve.extensions.push('.ts', '.tsx')
+          config.resolve.alias['@'] = path.resolve(__dirname)
+
+          return config
+        },
+      }
+    `)
+  }
+
+  console.log('')
+
+  log('Don\'t forget to run `npx eslint`', '')
 })
